fb4e7761f78ee94e999002fa19b4d5ba6a0af45c
Tu Dinh Tu <tudinhtu98@gmail.com>
2020-10-16 13:39:23 +0700

    build

--- dist/server.js
+++ dist/server.js
@@ -2,6 +2,28 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
 const PORT = 8080;
+app_1.default.get("/", (req, res) => {
+    res.send({
+        characters: [
+            {
+                name: "Nguyễn Văn A",
+                job: "Người của công chúng",
+            },
+            {
+                name: "Mac",
+                job: "Nhân viên Apple",
+            },
+            {
+                name: "Phượng Vĩ",
+                job: "Nhân viên Huawei",
+            },
+            {
+                name: "Obama",
+                job: "Former President",
+            },
+        ],
+    });
+});
 app_1.default.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
 });
